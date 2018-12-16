efbd3970d42fa320dc46d7560725fda388c31541
Mattias Karlsson <mattias.karlsson@careem.com>
2018-12-16 20:27:32 +0100

    Finish day 7.1

--- 7/main_2.py
+++ 7/main_2.py
@@ -1,11 +1,16 @@
+
+
+
+
 import numpy as np
 
 
 class Task:
 
-    def __init__(self, task):
+    def __init__(self, task, time_needed):
         self.task = task
         self.dependencies = set()
+        self.time_needed = time_needed
 
     def __eq__(self, other):
         if isinstance(other, Task):
@@ -41,10 +46,14 @@ with open('input.txt', 'r') as f:
     task_names = [t[7] for t in tasks]
     dependencies = [t[1] for t in tasks]
 
+ASCII_CHARACTERS = map(chr, range(65, 91))
+TIMES = 60 + np.array(range(1, 27))
+time_per_task = dict(zip(ASCII_CHARACTERS, TIMES))
+
 tasks = dict()
 for t in task_names + dependencies:
     if t not in tasks:
-        tasks[t] = Task(t)
+        tasks[t] = Task(t, time_per_task[t])
 
 for t, d in zip(task_names, dependencies):
     tasks[t].dependencies.add(tasks[d])
@@ -59,6 +68,9 @@ while len(tasks) > 0:
     tasks.pop(next_task.task)
     execution_order.append(next_task)
     progress.add(next_task)
+
+# TODO: Treat as matrix and iterate row by row, column is worker and row is
+# time.
 
 print(''.join([t.task for t in execution_order]))
 
